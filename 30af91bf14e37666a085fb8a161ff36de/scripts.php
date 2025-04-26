d0f44969f91153e9057540d607dccfd94593d1dd
rahulvijay073 <45328178+rahulvijay073@users.noreply.github.com>
2025-04-26 13:56:29 +0530

    script

--- 30af91bf14e37666a085fb8a161ff36de/scripts.php
+++ 30af91bf14e37666a085fb8a161ff36de/scripts.php
@@ -1,14 +1,22 @@
-    <script src="./assets/js/boot.min.js"></script>
-    <script src="./assets/js/jquery.min.js"></script>
-    <script src="./assets/js/dataTables.min.js"></script>
-    <script src="./assets/js/dataTables.bootstrap5.min.js"></script>
-    <script src="./assets/js/dataTables.buttons.min.js"></script>
-    <script src="./assets/js/buttons.bootstrap5.min.js"></script>
-    <script src="./assets/js/jszip.min.js"></script>
-    <script src="./assets/js/pdfmake.min.js"></script>
-    <script src="./assets/js/vfs_fonts.js"></script>
-    <script src="./assets/js/buttons.html5.min.js"></script>
-    <script src="./assets/js/buttons.print.min.js"></script>
-    <script src="./assets/js/sweetalert2@11.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/boot.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/dataTables.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/buttons.bootstrap5.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/jszip.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/pdfmake.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/vfs_fonts.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/buttons.print.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/js/sweetalert2@11.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/rahulvijay073/cdn-style@release/v1.1/30af91bf14e37666a085fb8a161ff36de/npm/chart.js"></script>
 
 
+
+
+    <script>
+        document.getElementById('sidebarToggle').addEventListener('click', function() {
+            document.getElementById('sidebar').classList.toggle('active');
+        });
+    </script>
