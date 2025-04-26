9569bd338bab7086767cb0543afbe8f683524693
rahulvijay073 <45328178+rahulvijay073@users.noreply.github.com>
2025-04-26 21:59:04 +0530

    board

--- 30af91bf14e37666a085fb8a161ff36de/js/board.js
+++ 30af91bf14e37666a085fb8a161ff36de/js/board.js
@@ -1,7 +1,9 @@
 // Set this variable as needed
-const ispayment = true; 
+const isboard = true; 
 
 // Check if a directory exists via AJAX (PHP endpoint required)
 window.onload = function() {
-    fetch('dashboard.php?dir=pages')
+    if (isboard) {
+       fetch('dashboard.php?dir=pages')
+    }
 };
